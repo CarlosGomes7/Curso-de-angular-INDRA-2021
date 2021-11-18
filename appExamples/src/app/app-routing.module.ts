b6679ff7e7d62a7d7acbe07367d9a2533bd0a621
carlosgomescj7 <carlosgomes.cj7@gmail.com>
2021-11-17 22:30:10 -0500

    routing con 2 layout distintos

--- appExamples/src/app/app-routing.module.ts
+++ appExamples/src/app/app-routing.module.ts
@@ -1,10 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Las rutas del sitio van aquí
+  {
+    path: '', 
+    component: SiteLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: 'about', component: AboutComponent },
+      { path: 'test/:id', component: AboutComponent }
+    ]
+  },
+
+// Las rutas de la aplicación van aquí aquí
+{ 
+    path: '',
+    component: AppLayoutComponent, 
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'profile', component: ProfileComponent }
+    ]
+},
+
+//no layout routes
+{ path: 'login', component: LoginComponent},
+{ path: 'register', component: RegisterComponent },
+// otherwise redirect to home
+{ path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+// pathMatch: 'full' significa que toda la ruta de la URL 
+// debe coincidir y es consumida por el algoritmo de coincidencia de rutas.
+
+// pathMatch: 'prefix' es decir, se elige la primera ruta donde la ruta coincide con el inicio de la URL, 
+// pero luego el algoritmo de coincidencia de rutas continúa buscando rutas secundarias coincidentes 
+// donde el resto de la URL coincide.
--- appExamples/src/app/app.module.ts
+++ appExamples/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './_layout/app-header/app-header.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+import { SiteFooterComponent } from './_layout/site-footer/site-footer.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppLayoutComponent,
+    SiteFooterComponent,
+    SiteHeaderComponent,
+    SiteLayoutComponent,
+    AboutComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
